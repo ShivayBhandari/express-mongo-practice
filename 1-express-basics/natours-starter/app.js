c5659ae4656ad78a229fbd9b5c5fcbc7f7d6256a
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-05-27 00:28:09 +0530

    Handling PATCH Requests

--- 1-express-basics/natours-starter/app.js
+++ 1-express-basics/natours-starter/app.js
@@ -22,7 +22,7 @@ app.get('/api/v1/tours', (req, res) => {
 app.get('/api/v1/tours/:id', (req, res) => {
   // : is used to declare a variable (id in this case). We can also define multiple varibales :id:x:y
   // add ? for optional parameters :id:x:y? now y is optional and id and x is compulsory
-  console.log(req.params); // all the parameters/variables are stored in req.params
+  // console.log(req.params); // all the parameters/variables are stored in req.params
   const id = req.params.id * 1; //req.param.id is in string format, convert to integer
   const tour = tours.find((el) => el.id === id); //Find function is used on arrays. Creates an array of elements which match the condotion given
 
@@ -35,7 +35,6 @@ app.get('/api/v1/tours/:id', (req, res) => {
 
   res.status(200).json({
     status: 'success',
-    results: tours.length,
     data: {
       tours: tour,
     },
@@ -58,6 +57,21 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID!',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
 });
 
 const port = 3000;
