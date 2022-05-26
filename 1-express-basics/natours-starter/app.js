da1c68dd829db8cf60f9018e2d1943dbd6932430
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-05-26 23:40:47 +0530

    Responding to URL parameters

--- 1-express-basics/natours-starter/app.js
+++ 1-express-basics/natours-starter/app.js
@@ -19,6 +19,29 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  // : is used to declare a variable (id in this case). We can also define multiple varibales :id:x:y
+  // add ? for optional parameters :id:x:y? now y is optional and id and x is compulsory
+  console.log(req.params); // all the parameters/variables are stored in req.params
+  const id = req.params.id * 1; //req.param.id is in string format, convert to integer
+  const tour = tours.find((el) => el.id === id); //Find function is used on arrays. Creates an array of elements which match the condotion given
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID!',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newID = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newID }, req.body);
