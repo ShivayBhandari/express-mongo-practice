b0bdda00f1e3e2685da9eae9cf81075a098f9750
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-06-12 16:10:13 +0530

    Creating a Simple Tour Model

--- 3-mongoose-mongodb/natours-starter/server.js
+++ 3-mongoose-mongodb/natours-starter/server.js
@@ -1,25 +1,42 @@
-const mongoose = require('mongoose'); // Requiring the mongoose
-const dotenv = require('dotenv'); // Requiring the dotenv module to use the config.env files
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.env' }); // importing the content of config.env
+dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE; // Declaring the DB variable
+const DB = process.env.DATABASE;
 mongoose
   .connect(DB, {
-    // Function to use mongodb with mongoose, first argument is the link
-    useNewUrlParser: true, // Second argument are some objects that are helped for not generating warnings
+    useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    //It returns a promise
     console.log('connected to DB');
   })
   .catch((err) => {
     console.log(err.message);
   });
+
+// Creating a new schema for mongoose
+// schema -> model -> model then used to interact with database
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'], // Schema options. Different for different types
+    unique: [true, 'A tour with same name already exists'], // cannot have 2 same names
+  },
+  rating: {
+    type: Number,
+    default: 4.5, // Default rating
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'], // Error message delcared in the form of list
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema); // Creating a model with schema. Naming convention of capital T
 
 const port = 3000;
 app.listen(port, () => {
