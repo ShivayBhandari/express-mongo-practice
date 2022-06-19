282b33f4be4ac92d56fcf979e342b6c14b41ce87
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-06-19 16:12:43 +0530

    Updating Documents Mongoose

--- 3-mongoose-mongodb/natours-starter/controllers/tourController.js
+++ 3-mongoose-mongodb/natours-starter/controllers/tourController.js
@@ -3,7 +3,7 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find(); //returns all the tour in tour collection. returns a promise
+    const tours = await Tour.find();
 
     res.status(200).json({
       status: 'success',
@@ -22,7 +22,7 @@ exports.getAllTours = async (req, res) => {
 
 exports.getTour = async (req, res) => {
   try {
-    const tour = await Tour.findById(req.params.id); //Find the element by ID
+    const tour = await Tour.findById(req.params.id);
     res.status(200).json({
       status: 'success',
       data: {
@@ -39,9 +39,6 @@ exports.getTour = async (req, res) => {
 
 exports.createTour = async (req, res) => {
   try {
-    // const newTour = new Tour({})
-    // newTour.save()
-    // worls same as above 2 lines. returns a promise
     const newTour = await Tour.create(req.body);
 
     res.status(201).json({
@@ -58,13 +55,25 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, //Always send the updated document to client
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour, //same as tour: tour
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
