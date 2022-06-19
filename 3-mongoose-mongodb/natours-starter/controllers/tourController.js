a0868c51501aa486f859e539f014c407419754d2
Shivay Bhandari <bhandarishivay2001@gmail.com>
2022-06-19 17:06:08 +0530

    Deleting Documents Mongoose

--- 3-mongoose-mongodb/natours-starter/controllers/tourController.js
+++ 3-mongoose-mongodb/natours-starter/controllers/tourController.js
@@ -58,7 +58,7 @@ exports.createTour = async (req, res) => {
 exports.updateTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-      new: true, //Always send the updated document to client
+      new: true,
       runValidators: true,
     });
 
@@ -76,9 +76,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
